18ab5752c7d221187a72439c82266e8cede587ce
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2023-10-11 13:50:06 -0300

    Make rabbitmq actually optional

--- gateway/src/main/java/org/georchestra/gateway/events/RabbitmqEventsAutoConfiguration.java
+++ gateway/src/main/java/org/georchestra/gateway/events/RabbitmqEventsAutoConfiguration.java
@@ -1,25 +1,29 @@
 package org.georchestra.gateway.events;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.amqp.core.Queue;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.gateway.config.GatewayAutoConfiguration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
 
 @Profile("!test && !it")
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration
 @AutoConfigureAfter(GatewayAutoConfiguration.class)
 @ImportResource({ "classpath:rabbit-listener-context.xml", "classpath:rabbit-sender-context.xml" })
-@ConditionalOnExpression("${georchestra.gateway.security.enableRabbitmqEvents:true}")
+@ConditionalOnProperty(name = "georchestra.gateway.security.enableRabbitmqEvents", havingValue = "true", matchIfMissing = false)
 public class RabbitmqEventsAutoConfiguration {
 
     @Bean
     @DependsOn({ "eventTemplate" })
-    public RabbitmqEventsSender eventsSender(AmqpTemplate eventTemplate) {
+    RabbitmqEventsSender eventsSender(AmqpTemplate eventTemplate) {
         return new RabbitmqEventsSender(eventTemplate);
     }
 
